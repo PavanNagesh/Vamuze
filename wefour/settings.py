b3249cae0a044d574e172c338abe5b75054bda61
PavanNagesh <108990790+PavanNagesh@users.noreply.github.com>
2024-04-28 13:54:15 +0530

    Update settings.py

--- wefour/settings.py
+++ wefour/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-6!#9tm*zian%p^f53dq^l&5cwz5y0@%!ko!r-alv@!hdvh&6q$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
