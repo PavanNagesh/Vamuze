bad5f85ca18816c995678e26302ef4406bf0a1e8
PavanNagesh <108990790+PavanNagesh@users.noreply.github.com>
2024-04-28 22:37:03 +0530

    Update settings.py

--- wefour/settings.py
+++ wefour/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'amuse.middleware.BruteForceMiddleware',
 ]
 
 ROOT_URLCONF = "wefour.urls"
