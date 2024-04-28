f207aab758f60abecaec3ac15ada18c3c1d68a69
PavanNagesh <108990790+PavanNagesh@users.noreply.github.com>
2024-04-28 22:47:50 +0530

    Update models.py

--- amuse/models.py
+++ amuse/models.py
@@ -3,42 +3,8 @@ from django.db import models
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
-    username = models.CharField(max_length=150, unique=True)  # Adding username field
+    username = models.CharField(max_length=150, unique=True)
     password = models.CharField(max_length=128)
 
-    def _str_(self):
+    def __str__(self):
         return self.username
-    
-    pass
-
-
-    # Define a custom intermediary model for the groups field
-    groups = models.ManyToManyField(
-        Group,
-        verbose_name='groups',
-        blank=True,
-        related_name='user_set_custom',
-        related_query_name='user_custom',
-        through='UserGroup',
-    )
-
-    # Define a custom intermediary model for the user_permissions field
-    user_permissions = models.ManyToManyField(
-        Permission,
-        verbose_name='user permissions',
-        blank=True,
-        related_name='user_set_custom',
-        related_query_name='user_custom',
-        through='UserPermission',
-    )
-
-# Custom intermediary model for the groups field
-class UserGroup(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    group = models.ForeignKey(Group, on_delete=models.CASCADE)
-
-
-# Custom intermediary model for the user_permissions field
-class UserPermission(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    permission = models.ForeignKey(Permission, on_delete=models.CASCADE)
