9941159836d6cf40ca3d3f9003d9cd6f4b816874
PavanNagesh <108990790+PavanNagesh@users.noreply.github.com>
2024-05-17 15:29:03 +0530

    Update urls.py

--- amuse/urls.py
+++ amuse/urls.py
@@ -45,7 +45,7 @@ urlpatterns = [
     path('userprofile/update', views.update, name='update'),
     path('cart', views.cart, name='cart'),
     path('add-to-cart/', views.add_to_cart, name='add_to_cart'),
-    path('update_cart/', views.update_cart, name='update_cart'),
+    path('update-cart/', views.update_cart, name='update_cart'),
     
 ]
 
